1c0302392a428fce26e77e4393e181ed69603093
Eric Lu <eric565648@gmail.com>
2021-11-10 16:45:29 -0500

    qp sort of working

--- oarbot_kinematics.m
+++ oarbot_kinematics.m
@@ -23,6 +23,8 @@ q1=0; q2=0; q3=0; q4=0; q5=0; q6=0; q7=0; q8=0; q9=0; q10=0;
 robot.H = [ex ey ez ez ez ey -ey -ex [-sin(pi/6); cos(pi/6); 0] -ex];
 robot.P = [0*ex 0*ex 0*ex l1*ex+l2*ez 0*ex 0*ex d2*ez d3*ex+e2*ey p89x*ex-p89y*ey 0*ex (d6+d4*sin(aa)/sin(2*aa))*ex];
 robot.joint_type = [1 1 0 1 0 0 0 0 0 0];
+robot.joint_upper_limit = [100 100 100 0.5 deg2rad([10000 130 180 10000 10000 10000])];
+robot.joint_lower_limit = [-100 -100 -100 0 deg2rad([-10000 -130 -71 -10000 -10000 -10000])];
 robot.q_zeros = [0 0 0 0 pi pi pi/2 0 0 0];
 
 robot.q = [q1 q2 q3 q4 q5 q6 q7 q8 q9 q10];
@@ -36,7 +38,7 @@ figure(1);show(robot_tree,(robot.q)','collision','on'); hold;
 
 % draw a convex shape as path
 x = [0.8, 3.5, 6.2];
-z = [1.11,1,1.11];
+z = [1.11,1.5,1.11];
 p = polyfit(x,z,2);
 x_end = 7.5; z_end=p(1)*x_end*x_end + p(2)*x_end + p(3);
 N = 100;
@@ -92,24 +94,22 @@ for i=1:S_length
     robot.T = T;
 
     if i==1
-        init_guess = [0 0 0 0 0 0 -pi/4 pi/6 pi/6 pi/6];
+        init_guess = [0 0 0 0 0 0 0 pi/6 pi/6 pi/6];
     else
         init_guess = all_sol(:,i-1)';
     end
 
-    %robot = invkiniter(robot, init_guess', 500, 1, [1 1 1 1 1 1], 0.01, 2);
+    %robot = invkiniter(robot, init_guess, 100, 1, [5 5 5 1 1 1], 0.01, 2);
+    robot = invkinqp(robot, init_guess, 1, 1, 0.01, 1, 2);
     
-    [q,solnInfo]=...
-        ik('body11',T,[1 1 1 1 1 1],init_guess');
+%     [q,solnInfo]=...
+%        ik('body11',T,[1 1 1 1 1 1],init_guess');
     
-    % all_sol = [all_sol robot.q'];
-    all_sol = [all_sol q];
-    p_error = norm(robot.T(1:3,4)-T(1:3,4));
-    r_error = norm(robot.T(1:3,1:3)*T(1:3,1:3-eye(3)),'fro');
-    all_error = [all_error [r_error;p_error]];
-    i
-    T(3,4)
-    figure(1);view(0,10);axis([-1 7 -1 1 -1 2]);show(robot_tree,all_sol(:,i),'collision','on');
+    all_sol = [all_sol robot.q'];
+%     all_sol = [all_sol q];
+    error_T = norm(robot.T-T,'fro');
+    all_error = [all_error error_T];
+    %figure(1);view(0,10);axis([-1 10 -1 1 -1 2]);show(robot_tree,all_sol(:,i),'collision','on');
 end
 
 for i=1:N
@@ -118,31 +118,71 @@ for i=1:N
         robot.q = all_sol(:,i)';
         robot = fwdkiniter(robot);
         
-        figure(1);axis([-1 7 -1 1 -1 2]);show(robot_tree,all_sol(:,i),'collision','on'); 
+        figure(1);axis([-1 9 -1 1 -1 2]);show(robot_tree,all_sol(:,i),'collision','on'); 
         pause(0.1);
         view(0,10);
     end
 end
 
+figure(2)
+plot(all_error, '*');
+title('Accracy of Method');
+ylabel('Forbenius error'); xlabel('index'); grid;
+
+function robot = invkinqp(robotd, init_guess, N, alpha, epis, Kp, type)
+    
+    robot_i = robotd;
+    q = init_guess';
+
+    for i=1:N
+        robot_i.q = q';
+        robot_i = fwdkiniter(robot_i);
+        dX = [s_err(robot_i.T(1:3,1:3)*(robotd.T(1:3,1:3))',type);(robot_i.T(1:3,4)-robotd.T(1:3,4))];
+        umax = (robot_i.joint_upper_limit-robot_i.q)'/alpha;
+        umin = (robot_i.joint_lower_limit-robot_i.q)'/alpha;
+        
+        upper = [1,1,2*pi,1,2*pi,2*pi,2*pi,2*pi,2*pi,2*pi]';
+        
+        umax = (umax>upper).*(upper)+(umax<=upper).*umax;
+        umin = (umin<-upper).*(-upper)+(umin>=-upper).*umin;
+        
+        %A = [diag(ones(10,1));diag(-1*ones(10,1))];
+        %b = [(robot_i.joint_upper_limit-robot_i.q)'/alpha; (-robot_i.joint_lower_limit+robot_i.q)'/alpha];
+
+        opt = optimset('display','off');
+        u = quadprog(robot_i.J'*robot_i.J,Kp*robot_i.J'*dX,[],[],[],[],umin,umax,q,opt);
+        %u = quadprog(robot_i.J'*robot_i.J,Kp*robot_i.J'*dX,A,b,[],[],[],[],q,opt);
+        %u = quadprog(robot_i.J'*robot_i.J,Kp*robot_i.J'*dX,[],[],[],[],[],[],q,opt);
+        q = q+alpha*u;
+        q(5:10)=(q(5:10)>=2*pi).*(-2*pi+q(5:10))+(q(5:10)<0).*(2*pi+q(5:10))+(q(5:10)<2*pi).*(q(5:10)>=0).*q(5:10);
+        q(3)=(q(3)>=2*pi).*(-2*pi+q(3))+(q(3)<0).*(2*pi+q(3))+(q(3)<2*pi).*(q(3)>=0).*q(3);
+    end
+    robot = robotd;
+    robot.q = q';
+    robot = fwdkiniter(robot);
+end
+
 function robot=invkiniter(robotd, init_guess, N, alpha, weight, epis, type)
 
     robot_i = robotd;
-    q = init_guess;
+    q = init_guess';
     
     for i=1:N
-        robot_i.q = q;
+        robot_i.q = q';
         robot_i = fwdkiniter(robot_i);
         
-        dX = weight'.*[s_err(robot_i.T(1:3,1:3)*robotd.T(1:3,1:3),type);(robot_i.T(1:3,4)-robotd.T(1:3,4))];
-        q = q-alpha*robot_i.J'*inv(robot_i.J*robot_i.J'+epis*eye(6))*dX;
+        dX = [s_err(robot_i.T(1:3,1:3)*(robotd.T(1:3,1:3))',type);(robot_i.T(1:3,4)-robotd.T(1:3,4))];
+        q = q-alpha*robot_i.J'*inv(robot_i.J*robot_i.J'+epis*diag(1./weight)*eye(6))*dX;
         % q = q-alpha*pinv(robot_i.J)*dX;
-        q=(q>=pi).*(-2*pi+q)+(q<-pi).*(2*pi+q)+(q<=pi).*(q>-pi).*q;
+        
+        
+        q(5:10)=(q(5:10)>=pi).*(-2*pi+q(5:10))+(q(5:10)<-pi).*(2*pi+q(5:10))+(q(5:10)<=pi).*(q(5:10)>-pi).*q(5:10);
+        q(3)=(q(3)>=pi).*(-2*pi+q(3))+(q(3)<-pi).*(2*pi+q(3))+(q(3)<=pi).*(q(3)>-pi).*q(3);
     end
     
     robot = robotd;
     robot.q = q';
     robot = fwdkiniter(robot);
-    robot = robot;
 end
 
 function robot=fwdkiniter(robot)
@@ -195,6 +235,13 @@ function err = s_err(er_mat, type)
     else
         error("Type is a interger of 1, 2 or 3");
     end
+
+end
+
+function s=sigmafun(hI,eta,c,M,e)
+
+    s=(hI>eta).*(-M*atan(c*(hI-eta))*2/pi)+...
+        (hI>=0).*(hI<eta).*(e*(eta-hI)/eta)+(hI<0).*e;
 
 end
 
