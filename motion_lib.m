b305ac7b397012cdd4a54b26f900ba5e312a4f16
Eric Lu <eric565648@gmail.com>
2021-11-30 00:11:16 -0500

    motion synthesis

--- motion_lib.m
+++ motion_lib.m
@@ -5,153 +5,314 @@ oarbot_param;
 
 kinova_param;
 
-sr = rand*0.3 + 0.45;stheta = rand*deg2rad(130)-deg2rad(60);
-sp = rand*deg2rad(90)-deg2rad(45);sy = rand*deg2rad(90)-deg2rad(45);sh = rand*0.5;
-er = rand*0.3 + 0.45;etheta = rand*deg2rad(130)-deg2rad(60);
-ep = rand*deg2rad(90)-deg2rad(45);ey = rand*deg2rad(90)-deg2rad(45);eh = rand*0.5;
-exp_rate=0.1; exp_r = 0.1;
-vel_type = 1;
-arm_speed=1;base_speed = 1;
+sr = rand*0.35 + 0.3;stheta = rand*deg2rad(150)-deg2rad(60);
+sp = rand*deg2rad(180)-deg2rad(90);sh = rand*0.5;
+er = rand*0.35 + 0.3;etheta = rand*deg2rad(150)-deg2rad(60);
+ep = rand*deg2rad(180)-deg2rad(90);eh = rand*0.5;
+exp_r = rand*2-3;
+arm_speed = rand-1;
+vel_type=1;
+base_speed=rand;
 
-sy = 0;ey=0;
+% testing
+% sr = 0.5; er=0.5;
+% stheta = deg2rad(-45);etheta = deg2rad(45);
+% sp = deg2rad(-45);ep = deg2rad(45);
+%sp=deg2rad(-45); ep=deg2rad(45); 
+%%%%
 
-param = [sr stheta sp sy sh er etheta ep ey eh exp_rate exp_r vel_type arm_speed base_speed];
-[path,pathp_d,pathq_d] = create_path(robot,armbot,sr,stheta,sp,sy,sh,er,etheta,ep,ey,eh,exp_rate,exp_r,vel_type,arm_speed,base_speed);
+param = [sr stheta sp sh er etheta ep eh exp_r vel_type arm_speed base_speed];
+[path,pathp_d,pathq_d] = create_path(robot,armbot,sr,stheta,sp,sh,er,etheta,ep,eh,exp_r,vel_type,arm_speed,base_speed);
 
-m = 10;
+m = int16(length(pathp_d(1,:))/20);
 figure(1);h=plotTransforms(pathp_d(:,1:m:end)',pathq_d(:,1:m:end)','FrameSize',0.3);
-view(31,12);
+view(43,16);
 
 err = [];
+
+tic
 for i=1:length(path(1,:))
-    % show robot pose (ever 5 frames)
-    robot.q = path(:,i)';
-    robot = fwdkiniter(robot);
-    dif = norm(robot.T-Transform(q2R(pathq_d(:,i)),pathp_d(:,i)),'fro');
-    err = [err dif];
-    if mod(i,5)==0
-        %figure(1);show(robot_tree,path(:,i),'collision','on');
-        figure(1);show(robot_tree,[0 0 0 0 0 0 0 -2.5261   -0.6155 0]','collision','on');
-        hold on; h=plotTransforms(pathp_d(:,1:m:end)',pathq_d(:,1:m:end)','FrameSize',0.3); hold off;
-        view(31,12);axis([0 1.5 -0.5 0.5 0 1.5]);
-        rad2deg(path(:,i))
-        pause(0.1);
-    end
+    % show robot pose (every 5 frames)
+%     robot.q = path(:,i)';
+%     robot = fwdkiniter(robot);
+%     dif = norm(robot.T-Transform(q2R(pathq_d(:,i)),pathp_d(:,i)),'fro');
+%     err = [err dif];
+%     if mod(i,1)==0
+    figure(1);show(robot_tree,path(:,i),'collision','on','PreservePlot',0,'FastUpdate',1);
+    %figure(1);show(robot_tree,[0 0 0 0 0 0 0 -2.5261   -0.6155 0]','collision','on');
+    %hold on; h=plotTransforms(pathp_d(:,1:m:end)',pathq_d(:,1:m:end)','FrameSize',0.3); hold off;
+    view(43,16);axis([0 5 -0.5 0.5 0 2]);
+%     view(88,11);axis([0 1.5 -0.5 0.5 0 1.5]);
+%     pause(0.2);
+%     end
+
+%     if i>1
+%         diff_q = path(5:end,i)-path(5:end,i-1);
+%         for j=1:length(diff_q)
+%             diff_q(j)=inpi(diff_q(j));
+%         end
+%         disp(diff_q);
+%         if abs(diff_q(4))>1
+%             %pause(10);
+%         end
+%     end
 end
+toc
+disp(length(path(1,:)))
 
-figure(2)
-plot(err, '*');
-title('Accracy of Method');
-ylabel('Forbenius error'); xlabel('index'); grid;
+% figure(2)
+% plot(err, '*');
+% title('Accracy of Method');
+% ylabel('Forbenius error'); xlabel('index'); grid;
 
-function [path,pathp_d,pathq_d] = create_path(robot,armbot,sr,stheta,sp,sy,sh,er,etheta,ep,ey,eh,exp_rate,exp_r,vel_type,arm_speed,base_speed)
+function [path,pathp_d,pathq_d] = create_path(robot,armbot,sr,stheta,sp,sh,er,etheta,ep,eh,exp_r,vel_type,arm_speed,base_speed)
 
-    N = 200; % TODO should related to velocity
+    %N = 50; % TODO should related to velocity
+    s_rate = 0.02; % sampling rate
 
     spoint = [sr*cos(stheta); 0; sr*sin(stheta)]+armbot.P(:,1);
     epoint = [er*cos(etheta); 0; er*sin(etheta)]+armbot.P(:,1);
-    armbot.q = armbot.home; armbot = fwdkiniter(armbot);
-    sspoint = armbot.T(1:3,4); ssrpy = R2rpy(armbot.T(1:3,1:3));
     
-    %ebx = base_speed*self.motion_T
-    ebx = 0;
-    eh=0.01;sh = 0.01;
+    %N = norm(epoint-spoint)/exp_r*exp_rate;
+    N = norm(epoint-spoint)*50;
+    
+    exp_r = 10^exp_r;
+    arm_speed = 10^arm_speed*2;
+    
 
-    path = zeros(length(robot.H),N+1);
+    path_p = [spoint];
+    for i=1:N
+        sl=epoint-spoint;
+        expp=sl*i/N+spoint;
+        expp_xz = randn*(exp_r/3);
+        th_xz = atan2(sl(3),sl(1))+pi/2;
+        expp_y = randn*(exp_r/3);
+        expp = expp + [expp_xz*cos(th_xz);expp_y;expp_xz*sin(th_xz)];
+        path_p = [path_p expp];
+    end
+    
+    path_vel_p = [spoint];
+    for i=1:N
+        j=1;
+        while 1
+            % propogate s_rate*arm_speed with in the direction
+            direction = (path_p(:,i+1)-path_p(:,i))/norm(path_p(:,i+1)-path_p(:,i));
+            this_p = path_p(:,i) + direction*(s_rate*arm_speed)*j;
+            
+            if norm(this_p-path_p(:,i)) > norm(path_p(:,i+1)-path_p(:,i))
+                break;
+            else
+                path_vel_p = [path_vel_p this_p];
+                j=j+1;
+            end
+        end
+    end
+    
+    path_type_p = [];
+    total_T = length(path_vel_p)*s_rate;
+    if total_T > 10 % at most 10 sec of a motion
+        total_T = 10;
+    end
+    total_N = total_T/s_rate;
+    acc_N = total_N/5;
+    acc_N_L = (total_N-acc_N)/2+1;
+    low_vel = 0.75;
+    high_vel = 1+(1-low_vel);
+    if vel_type == 1
+        path_type_p = path_vel_p;
+    elseif vel_type==2 || vel_type==3
+        if vel_type==2
+            vel_1=high_vel;vel_2=low_vel;
+        elseif vel_type==3
+            vel_1=low_vel;vel_2=high_vel;
+        end
+        
+        path_type_p = [path_type_p path_vel_p(:,1)];
+        pidx=1;
+        for i=2:acc_N_L
+            %pi = (i-1)*high_vel+1;
+            pidx = pidx+vel_1;
+            pi_i=floor(pidx); pi_d=pidx-floor(pidx);
+            this_p = path_vel_p(:,pi_i)+(path_vel_p(:,pi_i+1)-path_vel_p(:,pi_i))*pi_d;
+            path_type_p = [path_type_p this_p];
+        end
+        for i=acc_N_L+1:acc_N_L+acc_N
+            pidx = pidx+(vel_1-(vel_1-vel_2)*(i-acc_N_L)/acc_N);
+            pi_i=floor(pidx); pi_d=pidx-floor(pidx);
+            this_p = path_vel_p(:,pi_i)+(path_vel_p(:,pi_i+1)-path_vel_p(:,pi_i))*pi_d;
+            path_type_p = [path_type_p this_p];
+        end
+        for i=acc_N_L+1+acc_N:total_N
+            pidx = pidx+vel_2;
+            pi_i=floor(pidx); pi_d=pidx-floor(pidx);
+            if pi_i < length(path_vel_p(1,:))
+                this_p = path_vel_p(:,pi_i)+(path_vel_p(:,pi_i+1)-path_vel_p(:,pi_i))*pi_d;
+            else
+                this_p = path_vel_p(:,pi_i);
+            end
+            path_type_p = [path_type_p this_p];
+        end
+    elseif vel_type==4 || vel_type==5 || vel_type==6
+        if vel_type==4
+            vel_1=1;vel_2=1;
+        elseif vel_type==5
+            vel_1=high_vel;vel_2=low_vel;
+        elseif vel_type==6
+            vel_1=low_vel;vel_2=high_vel;
+        end
+        
+        path_type_p = [path_type_p path_vel_p(:,1)];
+        pidx=1;
+        for i=2:acc_N_L
+            pidx = pidx+vel_1;
+            pi_i=floor(pidx); pi_d=pidx-floor(pidx);
+            this_p = path_vel_p(:,pi_i)+(path_vel_p(:,pi_i+1)-path_vel_p(:,pi_i))*pi_d;
+            path_type_p = [path_type_p this_p];
+        end
+        for i=acc_N_L+1:acc_N_L+acc_N/3
+            pidx = pidx+(vel_1-(vel_1)*(i-acc_N_L)/(acc_N/3));
+            pi_i=floor(pidx); pi_d=pidx-floor(pidx);
+            this_p = path_vel_p(:,pi_i)+(path_vel_p(:,pi_i+1)-path_vel_p(:,pi_i))*pi_d;
+            path_type_p = [path_type_p this_p];
+        end
+        for i=acc_N_L+acc_N/3+1:acc_N_L+2*acc_N/3
+            pidx = pidx;
+            pi_i=floor(pidx); pi_d=pidx-floor(pidx);
+            this_p = path_vel_p(:,pi_i)+(path_vel_p(:,pi_i+1)-path_vel_p(:,pi_i))*pi_d;
+            path_type_p = [path_type_p this_p];
+        end
+        for i=acc_N_L+2*acc_N/3+1:acc_N_L+acc_N
+            pidx = pidx+(vel_2*(i-acc_N_L-2*acc_N/3)/(acc_N/3));
+            pi_i=floor(pidx); pi_d=pidx-floor(pidx);
+            this_p = path_vel_p(:,pi_i)+(path_vel_p(:,pi_i+1)-path_vel_p(:,pi_i))*pi_d;
+            path_type_p = [path_type_p this_p];
+        end
+        for i=acc_N_L+1+acc_N:total_N
+            pidx = pidx+vel_2;
+            pi_i=floor(pidx); pi_d=pidx-floor(pidx);
+            if pi_i < length(path_vel_p(1,:))
+                this_p = path_vel_p(:,pi_i)+(path_vel_p(:,pi_i+1)-path_vel_p(:,pi_i))*pi_d;
+            else
+                this_p = path_vel_p(:,pi_i);
+            end
+            path_type_p = [path_type_p this_p];
+        end
+    end
+    
+    path = [];
     pathp_d = [];
     pathq_d = [];
     
-    
-
-%     for i=1:N
-%         arm_p = (spoint-sspoint)*i/N+sspoint;
-%         %TODO:Add explore
-%         arm_rpy = (srpy-ssrpy)*i/N+ssrpy;
-%         arm_T = Transform(rpy2R(arm_rpy),arm_p);
-%         pathp_d = [pathp_d arm_T(1:3,4)+robot.P(:,4)-0.2755*[0;0;1]];pathq_d = [pathq_d R2q(arm_T(1:3,1:3))];
-%         
-%         armbot.T = arm_T;
-%         if i==1
-%             armbot = invkinqp(armbot,path(5:10,end)',2,1,0.01,1,2);
-%         else
-%             armbot = invkinqp(armbot,path(5:10,i-1)',2,1,0.01,1,2);
-%         end
-%         q = [0 0 0 0 armbot.q];
-%         path(:,i) = q;
-%     end
-    
-    arm_p = spoint;
-    %TODO:Add explore
-    arm_rpy = srpy;
-    arm_T = Transform(rpy2R(arm_rpy),arm_p);
-    pathp_d = [pathp_d arm_T(1:3,4)+robot.P(:,4)-0.2755*[0;0;1]+0.01];pathq_d = [pathq_d R2q(arm_T(1:3,1:3))];
-    armbot.T = arm_T;
-    %armbot = invkinqp(armbot,armbot.home,100,1,0.01,0.1,2);
-    armbot = invkiniter(armbot, armbot.home, 300, 1, [5 5 5 1 1 1], 0.01, 2);
-    q = [0 0 0 0 armbot.q];
-    path(:,1) = q;
-    
-    for i=1:N
-        arm_p = (epoint-spoint)*i/N+spoint;
-        %TODO:Add explore
-        arm_rpy = (erpy-srpy)*i/N+srpy;
-        arm_T = Transform(rpy2R(arm_rpy),arm_p);
+    %path_N = length(path_vel_p);
+    path_N = length(path_type_p);
+    for i=1:path_N
+        %arm_p = (epoint-spoint)*i/N+spoint;
+        %arm_p = path_p(:,i);
+        %arm_p = path_vel_p(:,i);
+        arm_p = path_type_p(:,i);
+        arm_pitch = (ep-sp)*i/path_N+sp;
+        arm_T = Transform(rpy2R([0;arm_pitch;0]),arm_p);
         pathp_d = [pathp_d arm_T(1:3,4)+robot.P(:,4)-0.2755*[0;0;1]+0.01];pathq_d = [pathq_d R2q(arm_T(1:3,1:3))];
         
         armbot.T = arm_T;
         %armbot = invkinqp(armbot,path(5:10,i)',70,1,0.01,0.1,2);
-        armbot = invkiniter(armbot, path(5:10,i)', 300, 1, [5 5 5 1 1 1], 0.01, 2);
-        sup_h = (eh-sh)/N*i+sh;
-        bx = ebx/N*i;
-        q = [bx 0 0 sup_h armbot.q];
-        path(:,i+1) = q;
+        % armbot = invkiniter(armbot, path(5:10,i)', 300, 1, [5 5 5 1 1 1], 0.01, 2);
+        
+        if i==1
+            qbot = invAly(armbot,arm_p,arm_pitch,[]);
+        else
+            qbot = invAly(armbot,arm_p,arm_pitch,path(5:end,end));
+        end
+        
+        sup_h = (eh-sh)/path_N*i+sh;
+        bx = base_speed*s_rate*(i-1);
+        if isempty(qbot)
+            disp('No solution')
+            path=[];
+            break;
+        end
+        q = [bx 0 0 sup_h qbot'];
+        path = [path q'];
+        disp('');
     end
 
 end
 
-function q=invAly(robot,pT,pitch,last)
+function qans=invAly(robot,pT,pitch,last)
 
     q = [];
 
     d = norm(pT-robot.P(:,1));
     p2 = robot.P(:,3);
-    p1 = robot.P4T;
+    p1 = -robot.P4T;
     k = robot.H(:,3);
     q3 = subprob3(d,p1,p2,k);
 
     for i=1:length(q3)
-        if q3(i)>robot.joint_upper_limit(3) or q3(i)<robot.joint_upper_limit(3)
+        if q3(i)>robot.joint_upper_limit(3) || q3(i)<robot.joint_lower_limit(3)
             continue;
         end
         R23 = rot(robot.H(:,3),q3(i));
         p1 = pT-robot.P(:,1);
         p2 = robot.P(:,3)+R23*robot.P4T;
-        k1 = -robot.H(:,1); k2=robot.H(:.2);
+        k1 = -robot.H(:,1); k2=robot.H(:,2);
         [q1,q2] = subprob2(p1,p2,k1,k2);
 
         for j=1:length(q1)
-            if q2(j)>robot.joint_upper_limit(2) or q2(j)<robot.joint_upper_limit(2)
+            if q2(j)>robot.joint_upper_limit(2) || q2(j)<robot.joint_lower_limit(2) || q1(j)>robot.joint_upper_limit(1) || q1(j)<robot.joint_lower_limit(1)
                 continue;
             end
             R01=rot(robot.H(:,1),q1(j)); R12=rot(robot.H(:,2),q2(j));
 
-            p1 = inv(R01*R12*R23)*[cos(pitch),0,sin(pitch)];
-            p2 = ex;
+            p1 = inv(R01*R12*R23)*[cos(-pitch);0;sin(-pitch)];
+            p2 = [1;0;0];
             k1 = -robot.H(:,4);k2=robot.H(:,5);
             [q4,q5] = subprob2(p1,p2,k1,k2);
 
             for l=1:length(q4)
-                if q4(l)>pi/2 or q4(l)<-pi/2
-                    continue;
-                end
-                R34=rot(robot.H(:,4),q4(l));R45=rot(robot.H(:,5),q5(l))
-                p1=ez; p2=inv(R01*R12*R23*R34*R45)*ey;
+%                 if q4(l)>pi/2 || q4(l)<-pi/2
+%                     continue;
+%                 end
+                R34=rot(robot.H(:,4),q4(l));R45=rot(robot.H(:,5),q5(l));
+                p1=[0;0;1]; p2=inv(R01*R12*R23*R34*R45)*[0;1;0];
                 k = robot.H(:,6);
                 q6 = subprob1(p1,p2,k);
                 q = [q [q1(j);q2(j);q3(i);q4(l);q5(l);q6]];
             end
         end
 
+    end
+    
+%     disp(length(q(1,:)))
+%     if length(q(1,:))>1
+%         disp("more than one ans");
+%     end
+    
+    if isempty(last) ~= 1
+        qi = 1;
+        sm=norm(q(:,qi)-last);
+        for i=1:length(q(1,:))
+            diffq = q(:,i)-last;
+            for j=1:length(diffq)
+                diffq(j)=inpi(diffq(j));
+            end
+            if norm(diffq)<sm
+                sm=norm(diffq);
+                qi=i;
+            end
+        end
+        qans = q(:,qi);
+    else
+        for i=1:length(q(1,:))
+            if q(4,i)<=pi/2 && q(4,i)>=-pi/2
+                qans = q(:,i);
+                break;
+            end
+        end
+    end
+    
+    for i=1:length(qans)
+        qans(i)=inpi(qans(i));
     end
 
 end
@@ -439,4 +600,14 @@ function q=subprob3(d, p1, p2, k)
 
     q = subprob3a(d_p, p1_p, p2_p, k);
 
+end
+
+function q_inpi=inpi(q)
+    while q > pi
+        q = q-2*pi;
+    end
+    while q < -pi
+        q = q+2*pi;
+    end
+    q_inpi = q;
 end
